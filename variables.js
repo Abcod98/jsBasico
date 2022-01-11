9bbe0ea01ea0d6ced12e41bfae608a372c8c00e5
Abraham <abcod98@gmail.com>
2022-01-11 11:51:08 -0600

    Variables

---
+++ variables.js
@@ -0,0 +1,17 @@
+var nombre = "Abraham";
+
+//declarar
+var edad;
+//inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora","Celular"];
+
+var persona = {
+    nombre: "Job",
+    edad: 23
+};
+
+persona;
