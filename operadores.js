3833fe9a8ad5af3e933ecaade4ec9dd069d0ed8b
Abraham <abcod98@gmail.com>
2022-01-13 10:55:37 -0600

    Operadores

---
+++ operadores.js
@@ -0,0 +1,43 @@
+// Binary operators, y esto es porque significa que estan 2 operandos involucrados
+
+// == De Operacion ==
+
+3 + 2
+50 -10
+10 * 3
+20 / 2
+
+'Abraham ' + 'Sanchez'
+
+// Unary operator, aqui es porque hay solo 1 operando involucrado
+
+// == Logicos ==
+
+!false // operador not (no)
+
+// == Asignacion ==
+
+var a = 1;
+
+// Comparacion
+
+3 == '3' // es igual
+
+3 === '3' // es estricamente igual
+
+5 < 3, 5 <= 3, 5 > 3, 5 >=3 // menor, menor o igual, mayor, mayor o igual
+
+a && b // operador and (y), este operador genera una validacion siempre y cuando ambas variables sean verdad, "var 1 y var 2"
+
+true || false // operador or (o), este operador genera una validacion siempre y cuando cualquiera de las variables sean veradad, "var 2 o var 2"
+
+/*=================================================================*/
+
+// Operador que solo se pueden utilizar con variables
+
+var edad = 40;
+
+edad++; // operador de incremento por 1
+edad += 2;
+
+edad;
