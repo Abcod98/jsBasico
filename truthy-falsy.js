f63d55971ef89c177df2e9dc7a153524f1b81ff3
Abraham <abcod98@gmail.com>
2022-01-13 09:56:24 -0600

    Truthy-Falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,27 @@
+// Truthy y Falsy
+
+// Son valores que son verdaderos o falsos
+
+// Falsy (False)
+
+"" // un string vacio
+0 -0
+null
+NaN
+false
+undefined
+
+// Truthy (true)
+
+"hola"
+30
+{a:1}
+[1,3]
+true
+function a (){}
+
+// Para que podemos utilizar esto, para generar condiciones segun el valor que demos.
+
+if(InputEvent.value){
+    //Codigo
+}
